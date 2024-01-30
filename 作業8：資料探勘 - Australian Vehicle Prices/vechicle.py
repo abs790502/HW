9af1e6e872a6699c26aa4d7ed76f431ad3989e7a
abs790502 <155990335+abs790502@users.noreply.github.com>
2024-01-31 00:10:30 +0800

    Update vechicle.py
    因為昨天的資料清洗詳細確認後，發現無法使用，故更新為此版本資料清洗進行數據分析

--- 作業8：資料探勘 - Australian Vehicle Prices/vechicle.py
+++ 作業8：資料探勘 - Australian Vehicle Prices/vechicle.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
 df = pd.read_csv('Australian Vehicle Prices.csv')
+# df.info()   最多有16733 比樣本
+#先查看缺少的值有多少
+# df.isnull().sum() 可以看到每行都有缺失值，其中Doors 與 Seats 缺少最多
 
-#print(df.info()) #由info 可看出缺少的項目 Car/Suv,Location,BodyType,Doors,Seats,Price
-#先處理Car/Suv,由model 中找出眾數填入
-
-#null_in_car_suv = ['02 **** ****\n','03 **** ****\n','07 **** ****\n','08 **** ****\n']    一開始因為沒有注意到這些字串後面有空格，導致卡關
-
-df.replace(r'\s+', '', regex=True, inplace=True)  # 去除所有空格
-null_in_car_suv = ['02********','03********','07********','08********']                                                                                                                                       
-df['Car/Suv'] = df['Car/Suv'].replace(null_in_car_suv, '')
+# df.dropna(inplace=True) #移除缺失值
+# df.isnull().sum() 移除後缺失值為0,但樣本數剩下14586比  ***要注意比原始資料少了12.8% ，此次分析不需要考慮Doors,Seats,Location 故先把這幾行的空格填入虛擬(virtual)
+df['Location'].fillna('virtual', inplace=True)
+df['Doors'].fillna('virtual', inplace=True)
+df['Seats'].fillna('virtual', inplace=True)
+df['Price'] = pd.to_numeric(df['Price'], errors='coerce') #price 欄位中有'POA' 故先把無效資訊轉成空值
+df.dropna(inplace=True)
+# df.info()   還有16389 比樣本 比原始資料少2% ，故將以此整理後資料進行數據分析
 
